7b2855f4d98b9b6dba536d37d84eccfc129f4774
Padam Sethia <padamsethia5@gmail.com>
2018-06-12 23:33:44 +0530

    Updated Forms and Models

--- models/login_model.py
+++ models/login_model.py
@@ -4,6 +4,10 @@ from flask_login import UserMixin
 from flask_wtf import FlaskForm 
 from app import db
 
+########################################
+#######  APP LOGIN FORMS & DB ##########
+########################################
+
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[InputRequired()])
     password = PasswordField('password', validators=[InputRequired()])
@@ -25,3 +29,59 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    
+
+
+########################################
+####### BASIC MASTER FORMS & DB ########
+########################################
+
+class Broker(db.Model):
+    pass
+
+class BrokerForm(FlaskForm):
+    pass
+
+class CommChannel(db.Model):
+    pass 
+
+class CommChannelForm(FlaskForm):
+    pass
+    
+class HealthChannel(db.Model):
+    pass 
+
+class HealthChannelForm(FlaskForm):
+    pass
+    
+class ProdCat(db.Model):
+    pass 
+
+class ProdCatForm(FlaskForm):
+    pass
+    
+class BussCat(db.Model):
+    pass 
+
+class BussCatForm(FlaskForm):
+    pass
+    
+########################################
+####### CONTACT FORMS & DB #############
+########################################
+
+class AddContact_form(FlaskForm):
+    pass
+
+class AddContact(db.Model):
+    id = db.Column(db.Integer , primary_key = True)
+    company_name = db.Column(db.String(50) , nullable = False)
+    contact_one = db.Column(db.String(20) , nullable = True)
+    wh_contact = db.Column(db.String(20) , nullable = True)
+    email = db.Column(db.String(50) , unique = True , nullable = True)
+    country = db.Column(db.String(30) , nullable = True)
+    state = db.Column(db.String(20) , nullable = True)
+    city = db.Column(db.String(20) , nullable = True)
+    buss_cat = db.Column(db.String(20) , nullable = True)
+    prod_cat = db.Column(db.String(20) , nullable = True)
+    broker = db.Column(db.String(20) , nullable = True)
+    comm_channel = db.Column(db.String(20) , nullable = True)
